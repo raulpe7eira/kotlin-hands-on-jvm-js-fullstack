5a13a4a7f83673bb5e8dc3ac6194b32361ca2db2
Raul Pereira <raul.pereira@gmail.com>
2020-07-26 16:16:32 -0300

    project setup (hello world)

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,35 @@
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CORS
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.gzip
+import io.ktor.http.HttpMethod
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.serialization.json
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 fun main() {
-    println("Hello, JVM!")
+    embeddedServer(Netty, 9090) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+        routing {
+            get("/hello") {
+                call.respondText("Hello, API!")
+            }
+        }
+    }.start(wait = true)
 }
