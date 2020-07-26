4d3c1879fa625ae4086804b5b0a131ec3da6352e
Raul Pereira <raul.pereira@gmail.com>
2020-07-26 16:45:16 -0300

    building the frontend

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -12,7 +12,11 @@ import io.ktor.http.content.static
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
-import io.ktor.routing.*
+import io.ktor.routing.delete
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+import io.ktor.routing.routing
 import io.ktor.serialization.json
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
